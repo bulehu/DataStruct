357010b4a70668bdb20902324c4462a261d430b2
bulehu <778797540@qq.com>
2021-01-17 20:54:39 +0800

    题目：删除链表中等于给定值 val 的所有节点

--- src/cn/ustc/singlylistnode/RemoveElements.java
+++ src/cn/ustc/singlylistnode/RemoveElements.java
@@ -34,4 +34,25 @@ public class RemoveElements {
         return head;
     }
 
+    /*
+    * 方法二：等同于双指针，但更加简洁
+    * */
+    public SinglyListNode removeElements2(SinglyListNode head, int val) {
+        //删除值相同的头结点后，可能新的头结点也值相等，用循环解决
+        while (head != null && head.val == val) {
+            head = head.next;
+        }
+        if (head == null)
+            return head;
+        SinglyListNode prev = head;
+        //确保当前结点后还有结点
+        while (prev.next != null) {
+            if (prev.next.val == val) {
+                prev.next = prev.next.next;
+            } else {
+                prev = prev.next;
+            }
+        }
+        return head;
+    }
 }
